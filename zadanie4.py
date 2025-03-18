b5419304c4d1c5a1c249a2edbea662060e900f58
fburczyk <filip.b0311@gmail.com>
2025-03-18 19:05:31 +0100

    Zmiana nazwy okna

--- zadanie4.py
+++ zadanie4.py
@@ -14,7 +14,7 @@ shifted = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
 
 translated_image_imutils = imutils.translate(image, 100, 50)
 
-cv2.imshow("Shifted", translated_image_imutils)
+cv2.imshow("Shifted with imutils", translated_image_imutils)
 cv2.imshow("Shifted", shifted)
 cv2.waitKey()
 cv2.destroyAllWindows()
